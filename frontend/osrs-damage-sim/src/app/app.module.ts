b29ef1a3b654bd7568293befd18c94786058c124
Maurits <63048736+Maurits825@users.noreply.github.com>
2022-10-01 23:04:36 +0200

    added general setup skeleton component

--- frontend/osrs-damage-sim/src/app/app.module.ts
+++ frontend/osrs-damage-sim/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgSelectModule } from '@ng-select/ng-select';
 import { AppComponent } from './app.component';
 import { GearSetupComponent } from './gear-setup/gear-setup.component';
+import { GeneralSetupComponent } from './general-setup/general-setup.component';
 import { WikiItemIconPipe } from './pipes/wiki-item-icon.pipe';
 import { DamageSimService } from './services/damage-sim.service';
 import { GearSetupService } from './services/gear-setups.service';
@@ -14,20 +15,25 @@ import { RlGearService } from './services/rl-gear.service';
   declarations: [
     AppComponent,
     GearSetupComponent,
-    WikiItemIconPipe
+    WikiItemIconPipe,
+    GeneralSetupComponent,
   ],
   imports: [
     NgSelectModule,
     FormsModule,
     ReactiveFormsModule,
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
   ],
   providers: [
     DamageSimService,
     RlGearService,
     GearSetupService,
   ],
-  bootstrap: [AppComponent, GearSetupComponent]
+  bootstrap: [
+    AppComponent,
+    GearSetupComponent,
+    GeneralSetupComponent,
+  ]
 })
 export class AppModule { }
---
+++ frontend/osrs-damage-sim/src/app/general-setup/general-setup.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-general-setup',
+  templateUrl: './general-setup.component.html',
+})
+export class GeneralSetupComponent implements OnInit {
+
+  constructor(
+    ) {}
+
+  ngOnInit(): void {
+  }
+}
