f42b2534fdc82c65b260f93465794f9850d66500
Maurits <63048736+Maurits825@users.noreply.github.com>
2022-10-14 17:49:01 +0200

    added d claws theoretical dps

--- backend/weapons/dragon_claws.py
+++ backend/weapons/dragon_claws.py
@@ -58,6 +58,45 @@ class DragonClaws(Weapon):
 
     def get_dps(self):
         if self.is_special_attack:
-            return 0
+            self.accuracy = self.get_accuracy()
+
+            avg_total_hit = 0
+
+            accuracy = self.accuracy
+            avg_hit1 = (math.floor(self.max_hit / 2) + (self.max_hit - 1)) / 2
+            avg_hit2 = avg_hit1 / 2
+            avg_hit3 = avg_hit2 / 2
+            avg_hit4 = avg_hit3 + 0.5
+            avg_total_hit += accuracy * (avg_hit1 + avg_hit2 + avg_hit3 + avg_hit4)
+
+            accuracy = (1 - self.accuracy) * self.accuracy
+            avg_hit1 = 0
+            avg_hit2 = (math.floor(self.max_hit * (3/8)) + math.floor(self.max_hit * (7/8))) / 2
+            avg_hit3 = avg_hit2 / 2
+            avg_hit4 = avg_hit3 + 0.5
+            avg_total_hit += accuracy * (avg_hit1 + avg_hit2 + avg_hit3 + avg_hit4)
+
+            accuracy = (1 - self.accuracy)**2 * self.accuracy
+            avg_hit1 = 0
+            avg_hit2 = 0
+            avg_hit3 = (math.floor(self.max_hit * (1/4)) + math.floor(self.max_hit * (3/4))) / 2
+            avg_hit4 = avg_hit3 + 0.5
+            avg_total_hit += accuracy * (avg_hit1 + avg_hit2 + avg_hit3 + avg_hit4)
+
+            accuracy = (1 - self.accuracy)**3 * self.accuracy
+            avg_hit1 = 0
+            avg_hit2 = 0
+            avg_hit3 = 0
+            avg_hit4 = (math.floor(self.max_hit * 0.25) + math.floor(self.max_hit * 1.25)) / 2
+            avg_total_hit += accuracy * (avg_hit1 + avg_hit2 + avg_hit3 + avg_hit4)
+
+            accuracy = (1 - self.accuracy)**4
+            avg_hit1 = 0
+            avg_hit2 = 0
+            avg_hit3 = 0.5
+            avg_hit4 = avg_hit3
+            avg_total_hit += accuracy * (avg_hit1 + avg_hit2 + avg_hit3 + avg_hit4)
+
+            return avg_total_hit / (self.attack_speed * 0.6)
         else:
-            return DpsCalculator.get_dps(self.max_hit, self.accuracy, self.attack_speed)
+            return super().get_dps()
