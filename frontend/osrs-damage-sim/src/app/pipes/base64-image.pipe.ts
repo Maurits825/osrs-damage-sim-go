54bf033c13e10a4dbb1f7b792be9f2209795d7cf
Maurits <63048736+Maurits825@users.noreply.github.com>
2023-01-30 15:30:09 +0100

    turn base64 img into blob

--- frontend/osrs-damage-sim/src/app/pipes/base64-image.pipe.ts
+++ frontend/osrs-damage-sim/src/app/pipes/base64-image.pipe.ts
@@ -7,6 +7,21 @@ import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 export class Base64ImagePipe implements PipeTransform {
   constructor(private sanitizer: DomSanitizer) {}
   transform(encodedImage: string): SafeResourceUrl {
-    return this.sanitizer.bypassSecurityTrustResourceUrl('data:image/png;base64,' + encodedImage);
+    var blob = this.dataURItoBlob(encodedImage);
+    var url = URL.createObjectURL(blob);
+    return this.sanitizer.bypassSecurityTrustUrl(url);
+  }
+
+  dataURItoBlob(dataURI: string) {
+    var byteString = window.atob(dataURI);
+
+    var ab = new ArrayBuffer(byteString.length);
+    var ia = new Uint8Array(ab);
+    for (var i = 0; i < byteString.length; i++) {
+      ia[i] = byteString.charCodeAt(i);
+    }
+
+    var blob = new Blob([ab], { type: 'image/png' });
+    return blob;
   }
 }
