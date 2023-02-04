a5e986be8e55aca0cedc712b7ec8c8f96270da4b
Maurits <63048736+Maurits825@users.noreply.github.com>
2023-02-05 00:26:30 +0100

    fix blowpipe id

--- frontend/osrs-damage-sim/src/app/gear-setup/gear-setup.const.ts
+++ frontend/osrs-damage-sim/src/app/gear-setup/gear-setup.const.ts
@@ -3,7 +3,7 @@ import { GearInputSetup } from '../model/input-setup.model';
 
 export const SPECIAL_BOLTS = [9242, 21944, 9243, 21946];
 export const DRAGON_DARTS_ID = 11230;
-export const BLOWPIPE_ID = 12926;
+export const BLOWPIPE_ID = 12924;
 export const UNARMED_EQUIVALENT_ID = 3689;
 
 export const DEFAULT_GEAR_SETUP: GearInputSetup = {
