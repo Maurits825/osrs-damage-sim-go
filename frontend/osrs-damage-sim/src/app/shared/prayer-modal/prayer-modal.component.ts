5c1bc4746e4d1b47e937746c78278f26930de4d5
Maurits <63048736+Maurits825@users.noreply.github.com>
2023-02-07 23:04:30 +0100

    wip prayer modal

--- frontend/osrs-damage-sim/src/app/shared/prayer-modal/prayer-modal.component.ts
+++ frontend/osrs-damage-sim/src/app/shared/prayer-modal/prayer-modal.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { allPrayers, Prayer } from 'src/app/model/osrs/prayer.model';
 
 @Component({
   selector: 'app-prayer-modal',
@@ -7,9 +8,26 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
   styleUrls: ['./prayer-modal.component.css'],
 })
 export class PrayerModalComponent implements OnInit {
-  @Input() name: any;
+  @Input()
+  selectedPrayers: Set<Prayer>;
+
+  @Output()
+  prayerToggle = new EventEmitter<Prayer>();
+
+  rows = Array(6)
+    .fill(0)
+    .map((_, i) => i);
+  cols = Array(5)
+    .fill(0)
+    .map((_, i) => i);
+
+  allPrayers = allPrayers;
 
   constructor(public activeModal: NgbActiveModal) {}
 
   ngOnInit(): void {}
+
+  togglePrayer(prayer: Prayer): void {
+    this.prayerToggle.emit(prayer);
+  }
 }
