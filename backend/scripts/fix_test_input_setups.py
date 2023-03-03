2a233e8b7a36000160c42cadc41768291f9c5825
Maurits <63048736+Maurits825@users.noreply.github.com>
2023-03-03 17:13:30 +0100

    updated unit test and split normal and spec tests

--- backend/scripts/fix_test_input_setups.py
+++ backend/scripts/fix_test_input_setups.py
@@ -1,90 +1,17 @@
 import json
 
-from wiki_data import WikiData
-
 
 def fix():
-    with open("tests/resources/input_setups.json") as f:
-        input_setups = json.load(f)
-        new_input_setups = {}
-        for setup_name in input_setups:
-            input_setup = input_setups[setup_name]
-            global_settings = {
-                "iterations": input_setup["iterations"],
-                "npcId": input_setup["npcId"],
-                "raidLevel": input_setup["raidLevel"],
-                "pathLevel": input_setup["pathLevel"],
-                "teamSize": input_setup["teamSize"],
-            }
-
-            new_input_setups[setup_name] = {
-                "expectedDps": input_setup["expectedDps"],
-                "globalSettings": global_settings,
-                "gearInputSetups": input_setup["gearInputSetups"],
-            }
-
-            for gear_input_setup in input_setup["gearInputSetups"]:
-                for gear_setup in gear_input_setup:
-                    new_gear = {}
-                    for gear_id in gear_setup["gear"]:
-                        item = WikiData.items_json[str(gear_id)]
-                        new_gear[str(item["slot"])] = {"id": gear_id}
-
-                    gear_setup["gear"] = new_gear
-                    gear_setup["setupName"] = gear_setup["name"]
-                    gear_setup["combatStats"]["hitpoints"] = 99
-                    del gear_setup["weapon"]
-                    del gear_setup["name"]
-
-    with open("tests/resources/input_setups_new.json", "w") as f:
-        f.write(json.dumps(new_input_setups))
-
-
-def fix2():
-    with open("tests/resources/input_setups.json") as f:
+    with open("tests/resources/spec_input_setups.json") as f:
         input_setups = json.load(f)
         new_input_setups = {}
         for setup_name in input_setups:
             old_input_setup = input_setups[setup_name]
-            new_input_setups[setup_name] = {
-                "globalSettings": old_input_setup["globalSettings"],
-                "expectedDps": old_input_setup["expectedDps"],
-            }
+            old_input_setup["inputGearSetups"][0]["fillGearSetups"] = [old_input_setup["inputGearSetups"][0]["fillGearSetups"]]
+            new_input_setups[setup_name] = old_input_setup
 
-            input_gear_setups = []
-            for gear_input_setup in old_input_setup["gearInputSetups"]:
-                input_gear_setup = {"gearSetupSettings": dict(), "gearSetups": gear_input_setup}
-                for gear_setup in gear_input_setup:
-                    input_gear_setup["gearSetupSettings"]["statDrains"] = []
-                    input_gear_setup["gearSetupSettings"]["combatStats"] = gear_setup["combatStats"]
-                    input_gear_setup["gearSetupSettings"]["boosts"] = gear_setup["boosts"]
-
-                input_gear_setups.append(input_gear_setup)
-
-            new_input_setups[setup_name]["inputGearSetups"] = input_gear_setups
-
-    with open("tests/resources/input_setups_new.json", "w") as f:
+    with open("tests/resources/spec_input_setups_new.json", "w") as f:
         f.write(json.dumps(new_input_setups))
 
 
-def fix3():
-    with open("tests/resources/input_setups.json") as f:
-        input_setups = json.load(f)
-        new_input_setups = {}
-        for setup_name in input_setups:
-            old_input_setup = input_setups[setup_name]
-
-            global_settings = old_input_setup["globalSettings"]
-            global_settings["npc"] = {"id": global_settings["npcId"]}
-            del global_settings["npcId"]
-
-            new_input_setups[setup_name] = {
-                "globalSettings": old_input_setup["globalSettings"],
-                "expectedDps": old_input_setup["expectedDps"],
-                "inputGearSetups": old_input_setup["inputGearSetups"]
-            }
-
-    with open("tests/resources/input_setups_new.json", "w") as f:
-        f.write(json.dumps(new_input_setups))
-
-fix3()
+fix()
---
+++ backend/tests/test_weapon_dps.py
@@ -0,0 +1,49 @@
+import json
+import unittest
+from pathlib import Path
+
+from damage_sim.damage_sim import DamageSim
+from input_setup.input_setup_converter import InputSetupConverter
+
+TEST_RESOURCE_FOLDER = Path(__file__).parent.parent / "tests/resources"
+
+
+class TestWeaponDps(unittest.TestCase):
+    input_setups: dict
+    spec_input_setups: dict
+
+    @classmethod
+    def setUpClass(cls):
+        with open(TEST_RESOURCE_FOLDER / "input_setups.json") as f:
+            TestWeaponDps.input_setups = json.load(f)
+
+        with open(TEST_RESOURCE_FOLDER / "spec_input_setups.json") as f:
+            TestWeaponDps.spec_input_setups = json.load(f)
+
+    def test_input_setups_dps(self):
+        print("Testing setup dps:")
+        for setup_name in TestWeaponDps.input_setups:
+            with self.subTest():
+                input_setup = InputSetupConverter.get_input_setup(TestWeaponDps.input_setups[setup_name])
+                damage_sim = DamageSim(input_setup.global_settings.npc, input_setup.input_gear_setups[0])
+                gear_setup_dps_stats = damage_sim.get_weapon_dps_stats()
+                dps = round(gear_setup_dps_stats.theoretical_dps[0], 8)
+
+                print(setup_name + " - " + str(dps))
+                self.assertEqual(TestWeaponDps.input_setups[setup_name]["expectedDps"], dps, setup_name)
+
+    def test_special_attack_input_setups_dps(self):
+        print("Testing spec setup dps:")
+        for setup_name in TestWeaponDps.spec_input_setups:
+            with self.subTest():
+                input_setup = InputSetupConverter.get_input_setup(TestWeaponDps.spec_input_setups[setup_name])
+                damage_sim = DamageSim(input_setup.global_settings.npc, input_setup.input_gear_setups[0])
+                gear_setup_dps_stats = damage_sim.get_weapon_dps_stats()
+                dps = round(gear_setup_dps_stats.theoretical_dps[1], 8)
+
+                print(setup_name + " - " + str(dps))
+                self.assertEqual(TestWeaponDps.spec_input_setups[setup_name]["expectedDps"], dps, setup_name)
+
+
+if __name__ == '__main__':
+    unittest.main()
