dc3ad665f6b5e2eee0e000580a08e79c077d8886
Maurits <63048736+Maurits825@users.noreply.github.com>
2023-03-05 23:07:54 +0100

    fixes for python 3.9

--- backend/condition_evaluator.py
+++ backend/condition_evaluator.py
@@ -4,17 +4,16 @@ from model.condition import ConditionComparison, Condition, ConditionVariables
 class ConditionEvaluator:
     @staticmethod
     def evaluate_comparison(comparison: ConditionComparison, value1, value2) -> bool:
-        match comparison:
-            case ConditionComparison.EQUAL:
-                return value1 == value2
-            case ConditionComparison.GRT_THAN:
-                return value1 > value2
-            case ConditionComparison.LESS_THAN:
-                return value1 < value2
-            case ConditionComparison.GRT_EQ_THAN:
-                return value1 >= value2
-            case ConditionComparison.LESS_EQ_THAN:
-                return value1 <= value2
+        if comparison == ConditionComparison.EQUAL:
+            return value1 == value2
+        elif comparison == ConditionComparison.GRT_THAN:
+            return value1 > value2
+        elif comparison == ConditionComparison.LESS_THAN:
+            return value1 < value2
+        elif comparison == ConditionComparison.GRT_EQ_THAN:
+            return value1 >= value2
+        elif comparison == ConditionComparison.LESS_EQ_THAN:
+            return value1 <= value2
 
     @staticmethod
     def evaluate_condition(conditions: list[Condition], npc_hitpoints, dmg_dealt, attack_count):
@@ -24,15 +23,14 @@ class ConditionEvaluator:
         bool_conditions = []
 
         for condition in conditions:
-            match condition.variable:
-                case ConditionVariables.NPC_HITPOINTS:
-                    variable = npc_hitpoints
-                case ConditionVariables.DMG_DEALT:
-                    variable = dmg_dealt
-                case ConditionVariables.ATTACK_COUNT:
-                    variable = attack_count
-                case _:
-                    variable = 0
+            if condition == ConditionVariables.NPC_HITPOINTS:
+                variable = npc_hitpoints
+            elif condition == ConditionVariables.DMG_DEALT:
+                variable = dmg_dealt
+            elif condition == ConditionVariables.ATTACK_COUNT:
+                variable = attack_count
+            else:
+                variable = 0
 
             bool_conditions.append(ConditionEvaluator.evaluate_comparison(condition.comparison,
                                                                           variable, condition.value))
@@ -41,10 +39,9 @@ class ConditionEvaluator:
         condition_result = bool_conditions[0]
         for idx, condition in enumerate(conditions[:-1]):
             condition_result = True
-            match condition.nextComparison:
-                case ConditionComparison.AND:
-                    condition_result = condition_result and (bool_conditions[idx] and bool_conditions[idx + 1])
-                case ConditionComparison.OR:
-                    condition_result = condition_result and (bool_conditions[idx] or bool_conditions[idx + 1])
+            if condition.nextComparison == ConditionComparison.AND:
+                condition_result = condition_result and (bool_conditions[idx] and bool_conditions[idx + 1])
+            elif condition.nextComparison == ConditionComparison.OR:
+                condition_result = condition_result and (bool_conditions[idx] or bool_conditions[idx + 1])
 
         return condition_result
