d69282e4848b0fd2fe83323fa2a3ee7be0bfa902
Maurits <63048736+Maurits825@users.noreply.github.com>
2023-03-06 20:19:24 +0100

    added production url and zappa settings

--- frontend/osrs-damage-sim/src/environments/environment.prod.ts
+++ frontend/osrs-damage-sim/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  OSRS_DAMAGE_SIM_SERVICE_URL: 'https://p8xwdgfv0g.execute-api.eu-central-1.amazonaws.com/prod',
+  OSRS_DAMAGE_SIM_SERVICE_URL: 'https://q1o30bnxvk.execute-api.eu-central-1.amazonaws.com/production',
 };
