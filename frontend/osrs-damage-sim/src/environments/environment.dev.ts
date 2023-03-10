e51b4a2385bf73c2d3bc47d414f7836154447514
Maurits <63048736+Maurits825@users.noreply.github.com>
2023-03-10 18:41:43 +0100

    added prod/dev/localhost env for FE

---
+++ frontend/osrs-damage-sim/src/environments/environment.dev.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  OSRS_DAMAGE_SIM_SERVICE_URL: 'https://gec3ikadr5.execute-api.eu-central-1.amazonaws.com/dev',
+};
---
+++ frontend/osrs-damage-sim/src/environments/environment.localhost.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  OSRS_DAMAGE_SIM_SERVICE_URL: 'http://localhost:5000',
+};
