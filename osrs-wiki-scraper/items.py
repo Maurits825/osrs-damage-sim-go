e036806f89c43bc834091e50c4aaddae68a2d8e6
Maurits <63048736+Maurits825@users.noreply.github.com>
2023-03-12 19:55:50 +0100

    reformat files

--- osrs-wiki-scraper/items.py
+++ osrs-wiki-scraper/items.py
@@ -11,131 +11,135 @@ from constants import CACHE_DATA_FOLDER
 
 "this isn't quite right, because 2h, but the format isn't smart enough for that"
 slotIDs: Dict[str, int] = {
-	"weapon": 3,
-	"2h": 3,
-	"body": 4,
-	"head": 0,
-	"ammo": 13,
-	"legs": 7,
-	"feet": 10,
-	"hands": 9,
-	"cape": 1,
-	"neck": 2,
-	"ring": 12,
-	"shield": 5
+    "weapon": 3,
+    "2h": 3,
+    "body": 4,
+    "head": 0,
+    "ammo": 13,
+    "legs": 7,
+    "feet": 10,
+    "hands": 9,
+    "cape": 1,
+    "neck": 2,
+    "ring": 12,
+    "shield": 5
 }
 
+
 def getLimits():
-	req = urllib.request.Request(
-		'https://oldschool.runescape.wiki/w/Module:GELimits/data?action=raw', headers=api.user_agent)
-	with urllib.request.urlopen(req) as response:
-		data = response.read()
-	limits = {}
-	for line in data.splitlines():
-		match = re.search(r"\[\"(.*)\"\] = (\d+),?", str(line))
-		if match:
-			name = match.group(1).replace('\\', '')
-			limit = match.group(2)
-			limits[name] = int(limit)
-	return limits
+    req = urllib.request.Request(
+        'https://oldschool.runescape.wiki/w/Module:GELimits/data?action=raw', headers=api.user_agent)
+    with urllib.request.urlopen(req) as response:
+        data = response.read()
+    limits = {}
+    for line in data.splitlines():
+        match = re.search(r"\[\"(.*)\"\] = (\d+),?", str(line))
+        if match:
+            name = match.group(1).replace('\\', '')
+            limit = match.group(2)
+            limits[name] = int(limit)
+    return limits
 
 
 def run():
-	limits = getLimits()
-	stats = {}
+    limits = getLimits()
+    stats = {}
 
-	item_pages = api.query_category("Items")
-	for name, page in item_pages.items():
-		if name.startswith("Category:"):
-			continue
+    item_pages = api.query_category("Items")
+    for name, page in item_pages.items():
+        if name.startswith("Category:"):
+            continue
 
-		try:
-			code = mw.parse(page, skip_style_tags=True)
+        try:
+            code = mw.parse(page, skip_style_tags=True)
 
-			equips = {}
-			for (vid, version) in util.each_version("Infobox Bonuses", code, include_base=True):
-				doc = {}
-				equips[vid] = doc
+            equips = {}
+            for (vid, version) in util.each_version("Infobox Bonuses", code, include_base=True):
+                doc = {}
+                equips[vid] = doc
 
-				if "slot" not in version:
-					continue
+                if "slot" not in version:
+                    continue
 
-				slotID = str(version["slot"]).strip().lower()
-				if slotID in slotIDs:
-					doc["slot"] = slotIDs[slotID]
-					if slotID == "2h":
-						doc["is2h"] = True
-				else:
-					print("Item {} has unknown slot {}".format(name, slotID))
-					continue
+                slotID = str(version["slot"]).strip().lower()
+                if slotID in slotIDs:
+                    doc["slot"] = slotIDs[slotID]
+                    if slotID == "2h":
+                        doc["is2h"] = True
+                else:
+                    print("Item {} has unknown slot {}".format(name, slotID))
+                    continue
 
-				for key in [
-					"astab", "aslash", "acrush", "amagic", "arange", "dstab", "dslash", "dcrush", "dmagic", "drange", "str",
-					"rstr", "mdmg", "prayer", ("speed", "aspeed")
-				]:
-					try:
-						util.copy(key, doc, version, lambda x: int(x))
-					except ValueError:
-						print("Item {} has an non integer {}".format(name, key))
+                for key in [
+                    "astab", "aslash", "acrush", "amagic", "arange", "dstab", "dslash", "dcrush", "dmagic", "drange",
+                    "str",
+                    "rstr", "mdmg", "prayer", ("speed", "aspeed")
+                ]:
+                    try:
+                        util.copy(key, doc, version, lambda x: int(x))
+                    except ValueError:
+                        print("Item {} has an non integer {}".format(name, key))
 
-			for (vid, version) in util.each_version("Infobox Item", code, mergable_keys=None if len(equips) <= 1 else []):
-				if "removal" in version and not str(version["removal"]).strip().lower() in ["", "no", "n/a"]:
-					continue
+            for (vid, version) in util.each_version("Infobox Item", code,
+                                                    mergable_keys=None if len(equips) <= 1 else []):
+                if "removal" in version and not str(version["removal"]).strip().lower() in ["", "no", "n/a"]:
+                    continue
 
-				equipable = "equipable" in version and "yes" in str(version["equipable"]).strip().lower()
-				equipVid = vid if vid in equips else -1 if -1 in equips else None
-				if not equipable or equipVid is None:
-					continue
+                equipable = "equipable" in version and "yes" in str(version["equipable"]).strip().lower()
+                equipVid = vid if vid in equips else -1 if -1 in equips else None
+                if not equipable or equipVid is None:
+                    continue
 
-				doc = util.get_doc_for_id_string(name + str(vid), version, stats)
-				if doc == None:
-					continue
+                doc = util.get_doc_for_id_string(name + str(vid), version, stats)
+                if doc == None:
+                    continue
 
-				util.copy("name", doc, version)
-				if not "name" in doc:
-					doc["name"] = name
+                util.copy("name", doc, version)
+                if not "name" in doc:
+                    doc["name"] = name
 
-				doc["name"] = name
+                doc["name"] = name
 
-				util.copy("quest", doc, version, lambda x: x.lower() != "no")
+                util.copy("quest", doc, version, lambda x: x.lower() != "no")
 
-				equipable = "equipable" in version and "yes" in str(version["equipable"]).strip().lower()
+                equipable = "equipable" in version and "yes" in str(version["equipable"]).strip().lower()
 
-				if "weight" in version:
-					strval = str(version["weight"]).strip()
-					strval = re.sub(r"<!--.*?-->", "", strval)
-					if strval.endswith("kg"):
-						strval = strval[:-2].strip()
-					if strval != "":
-						floatval = float(strval)
-						if floatval != 0:
-							doc["weight"] = floatval
+                if "weight" in version:
+                    strval = str(version["weight"]).strip()
+                    strval = re.sub(r"<!--.*?-->", "", strval)
+                    if strval.endswith("kg"):
+                        strval = strval[:-2].strip()
+                    if strval != "":
+                        floatval = float(strval)
+                        if floatval != 0:
+                            doc["weight"] = floatval
 
-				if equipVid != None and (equipable or not "broken" in version["name"].lower()):
-					if not equipable:
-						print("Item {} has Infobox Bonuses but not equipable".format(name))
-						continue
-					doc["equipable"] = True
-					for k in equips[equipVid]:
-						doc[k] = equips[equipVid][k]
-				else:
-					continue
+                if equipVid != None and (equipable or not "broken" in version["name"].lower()):
+                    if not equipable:
+                        print("Item {} has Infobox Bonuses but not equipable".format(name))
+                        continue
+                    doc["equipable"] = True
+                    for k in equips[equipVid]:
+                        doc[k] = equips[equipVid][k]
+                else:
+                    continue
 
-				itemName = name
-				if "gemwname" in version:
-					itemName = str(version["gemwname"]).strip()
-				elif "name" in version:
-					itemName = str(version["name"]).strip()
-				if itemName in limits:
-					doc['ge_limit'] = limits[itemName]
+                itemName = name
+                if "gemwname" in version:
+                    itemName = str(version["gemwname"]).strip()
+                elif "name" in version:
+                    itemName = str(version["name"]).strip()
+                if itemName in limits:
+                    doc['ge_limit'] = limits[itemName]
 
-				for (vid, version) in util.each_version("CombatStyles", code):
-					doc['weaponCategory'] = version[''].upper().replace(' ', '_').replace('2H_SWORD', 'TWO_HANDED_SWORD')
+                for (vid, version) in util.each_version("CombatStyles", code):
+                    doc['weaponCategory'] = version[''].upper().replace(' ', '_').replace('2H_SWORD',
+                                                                                          'TWO_HANDED_SWORD')
 
-		except (KeyboardInterrupt, SystemExit):
-			raise
-		except:
-			print("Item {} failed:".format(name))
-			traceback.print_exc()
+        except (KeyboardInterrupt, SystemExit):
+            raise
+        except:
+            print("Item {} failed:".format(name))
+            traceback.print_exc()
 
-	util.write_json(CACHE_DATA_FOLDER / "items-dmg-sim.json", CACHE_DATA_FOLDER / "items-dmg-sim.min.json", stats)
+    util.write_json(CACHE_DATA_FOLDER / "items-dmg-sim.json", CACHE_DATA_FOLDER / "items-dmg-sim.min.json", stats)
--- osrs-wiki-scraper/npcs.py
+++ osrs-wiki-scraper/npcs.py
@@ -11,61 +11,61 @@ from tob_npcs import TobNpcs
 
 # Modification here to include many more attributes
 npc_trait_keys = ["hitpoints", "att", "str", "def", "mage", "range", "attbns", "strbns", "defbns", "amagic", "mbns",
-				  "arange", "rngbns", "dstab", "dslash", "dcrush", "dmagic", "drange", "combat", "size"]
+                  "arange", "rngbns", "dstab", "dslash", "dcrush", "dmagic", "drange", "combat", "size"]
 
 
 def run():
-	npcs = {}
+    npcs = {}
 
-	npc_pages = api.query_category("Monsters")
-	for name, page in npc_pages.items():
-		if name.startswith("Category:"):
-			continue
+    npc_pages = api.query_category("Monsters")
+    for name, page in npc_pages.items():
+        if name.startswith("Category:"):
+            continue
 
-		try:
-			code = mw.parse(page, skip_style_tags=True)
+        try:
+            code = mw.parse(page, skip_style_tags=True)
 
-			for (vid, version) in util.each_version("Infobox Monster", code):
-				if "removal" in version and not str(version["removal"]).strip().lower() in ["", "no"]:
-					continue
+            for (vid, version) in util.each_version("Infobox Monster", code):
+                if "removal" in version and not str(version["removal"]).strip().lower() in ["", "no"]:
+                    continue
 
-				is_cox = util.has_template("Chambers of Xeric", code)
-				is_tob = util.has_template("Theatre of Blood", code)
+                is_cox = util.has_template("Chambers of Xeric", code)
+                is_tob = util.has_template("Theatre of Blood", code)
 
-				if is_cox:
-					doc, custom_name = CoxNpcs.run(name + str(vid), vid, version, npcs)
-				elif is_tob:
-					doc, custom_name = TobNpcs.run(name + str(vid), vid, version, npcs)
-				else:
-					doc = util.get_doc_for_id_string(name + str(vid), version, npcs)
-					custom_name = CustomNames.get_custom_name(version)
+                if is_cox:
+                    doc, custom_name = CoxNpcs.run(name + str(vid), vid, version, npcs)
+                elif is_tob:
+                    doc, custom_name = TobNpcs.run(name + str(vid), vid, version, npcs)
+                else:
+                    doc = util.get_doc_for_id_string(name + str(vid), version, npcs)
+                    custom_name = CustomNames.get_custom_name(version)
 
-				if not doc:
-					continue
+                if not doc:
+                    continue
 
-				util.copy("name", doc, version)
-				if "name" not in doc:
-					doc["name"] = name
+                util.copy("name", doc, version)
+                if "name" not in doc:
+                    doc["name"] = name
 
-				if "attributes" in version:
-					attrs = [x.strip() for x in version["attributes"].split(",") if x.strip()]
-					for attr in attrs:
-						doc[f"is{attr[0].upper()}{attr[1:]}"] = True
+                if "attributes" in version:
+                    attrs = [x.strip() for x in version["attributes"].split(",") if x.strip()]
+                    for attr in attrs:
+                        doc[f"is{attr[0].upper()}{attr[1:]}"] = True
 
-				if is_cox or is_tob or custom_name:
-					doc["name"] = custom_name
+                if is_cox or is_tob or custom_name:
+                    doc["name"] = custom_name
 
-				for key in npc_trait_keys:
-					try:
-						util.copy(key, doc, version, lambda x: int(x))
-					except ValueError:
-						pass
-						#print("NPC {} has an non integer {}".format(name, key))
+                for key in npc_trait_keys:
+                    try:
+                        util.copy(key, doc, version, lambda x: int(x))
+                    except ValueError:
+                        pass
+                # print("NPC {} has an non integer {}".format(name, key))
 
-		except (KeyboardInterrupt, SystemExit):
-			raise
-		except:
-			print("NPC {} failed:".format(name))
-			traceback.print_exc()
+        except (KeyboardInterrupt, SystemExit):
+            raise
+        except:
+            print("NPC {} failed:".format(name))
+            traceback.print_exc()
 
-	util.write_json(CACHE_DATA_FOLDER / "npcs-dmg-sim.json", CACHE_DATA_FOLDER / "npcs-dmg-sim.min.json", npcs)
+    util.write_json(CACHE_DATA_FOLDER / "npcs-dmg-sim.json", CACHE_DATA_FOLDER / "npcs-dmg-sim.min.json", npcs)
