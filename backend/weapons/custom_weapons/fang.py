6ea85f18cf6e703a636510cec51376072f279ef6
Maurits <63048736+Maurits825@users.noreply.github.com>
2023-03-20 00:22:15 +0100

    fixed fang

--- backend/weapons/custom_weapons/fang.py
+++ backend/weapons/custom_weapons/fang.py
@@ -12,22 +12,19 @@ class Fang(Weapon):
     def __init__(self, gear_setup: GearSetup, combat_stats: CombatStats, npc: NpcStats, raid_level):
         super().__init__(gear_setup, combat_stats, npc, raid_level)
 
-        self.true_min_hit = Fang.get_true_min_hit(self.max_hit)
-        self.true_max_hit = Fang.get_true_max_hit(self.max_hit)
-
-    def set_combat_stats(self, combat_stats):
-        super().set_combat_stats(combat_stats)
+        self.true_min_hit = 0
 
     def roll_hit(self) -> bool:
+        max_defence_roll = self.get_defence_roll()
         attack_roll = int(random.random() * (self.attack_roll + 1))
-        defence_roll = int(random.random() * (self.get_defence_roll() + 1))
+        defence_roll = int(random.random() * (max_defence_roll + 1))
 
         if attack_roll > defence_roll:
             return True
 
         if self.raid_level:
             attack_roll = int(random.random() * (self.attack_roll + 1))
-            defence_roll = int(random.random() * (self.get_defence_roll() + 1))
+            defence_roll = int(random.random() * (max_defence_roll + 1))
             return attack_roll > defence_roll
         else:
             attack_roll = int(random.random() * (self.attack_roll + 1))
@@ -36,8 +33,6 @@ class Fang(Weapon):
     def roll_damage(self) -> int:
         damage = 0
         max_hit = self.max_hit
-        if not self.gear_setup.is_special_attack:
-            max_hit = self.true_max_hit
 
         if self.roll_hit():
             damage = int((random.random() * (max_hit - self.true_min_hit + 1)) + self.true_min_hit)
@@ -52,6 +47,24 @@ class Fang(Weapon):
 
     def get_dps(self):
         accuracy = self.get_accuracy()
+        max_hit = super().get_max_hit()
+
+        if self.gear_setup.is_special_attack:
+            max_hit += self.get_true_min_hit()
+
+        return DpsCalculator.get_dps(max_hit, accuracy, self.gear_setup.gear_stats.attack_speed)
+
+    def get_max_hit(self):
+        max_hit = super().get_max_hit()
+        self.true_min_hit = self.get_true_min_hit()
+
+        if self.gear_setup.is_special_attack:
+            return max_hit
+        else:
+            return max_hit - math.floor(max_hit * 0.15)
+
+    def get_accuracy(self):
+        accuracy = super().get_accuracy()
         attack_roll = self.get_attack_roll()
 
         if self.raid_level:
@@ -65,18 +78,7 @@ class Fang(Weapon):
                 effective_accuracy = ((attack_roll * (4 * attack_roll + 5)) /
                                       (6 * (attack_roll + 1) * (defence_roll + 1)))
 
-        max_hit = self.get_max_hit()
-        min_hit = Fang.get_true_min_hit(max_hit)
-        if not self.gear_setup.is_special_attack:
-            max_hit = Fang.get_true_max_hit(max_hit)
+        return effective_accuracy
 
-        effective_max_hit = min_hit + max_hit
-        return DpsCalculator.get_dps(effective_max_hit, effective_accuracy, self.gear_setup.gear_stats.attack_speed)
-
-    @staticmethod
-    def get_true_min_hit(max_hit):
-        return math.floor(max_hit * 0.15)
-
-    @staticmethod
-    def get_true_max_hit(max_hit):
-        return max_hit - math.floor(max_hit * 0.15)
+    def get_true_min_hit(self):
+        return math.floor(super().get_max_hit() * 0.15)
