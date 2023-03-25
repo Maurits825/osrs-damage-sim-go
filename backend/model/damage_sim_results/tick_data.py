ebe47a351de2da645c5017b26263b577573652ba
Maurits <63048736+Maurits825@users.noreply.github.com>
2023-03-25 15:32:54 +0100

    added some more fields to tick data

--- backend/model/damage_sim_results/tick_data.py
+++ backend/model/damage_sim_results/tick_data.py
@@ -1,8 +1,16 @@
 from dataclasses import dataclass
 
+from model.npc.npc_stats import NpcStats
 from weapons.weapon import Weapon
 
 
 @dataclass()
 class TickData:
-    current_weapon: Weapon
+    weapon: Weapon
+    accuracy: float  # this is in weapon no??
+    max_hit: int  # this is in weapon no??
+    damage_dealt: int
+
+    npc: NpcStats
+
+    special_attack_amount: float
