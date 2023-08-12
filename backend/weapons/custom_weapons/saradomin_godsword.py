2d102642cdc358c81c3f7f1bd4773da41f538d63
Maurits <mauritsvdsteur@live.com>
2023-08-12 19:45:51 +0200

    slash roll on sgs spec

--- backend/weapons/custom_weapons/saradomin_godsword.py
+++ backend/weapons/custom_weapons/saradomin_godsword.py
@@ -16,3 +16,11 @@ class SaradominGodsword(Weapon):
             return super().get_attack_roll() * 2
         else:
             return super().get_attack_roll()
+
+    def get_npc_defence_and_style(self):
+        if not self.gear_setup.is_special_attack:
+            return super().get_npc_defence_and_style()
+
+        target_defence = self.npc.combat_stats.defence
+        target_defence_style = self.npc.defensive_stats.slash
+        return target_defence, target_defence_style
