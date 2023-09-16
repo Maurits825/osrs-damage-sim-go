58476071b3212d1249fd23c43b5278146adbf005
Maurits <63048736+Maurits825@users.noreply.github.com>
2023-09-16 17:59:00 +0200

    remove chart js stuff

--- frontend/osrs-damage-sim/src/app/core/dps-grapher/dps-grapher.component.ts
+++ frontend/osrs-damage-sim/src/app/core/dps-grapher/dps-grapher.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Chart } from 'chart.js/auto';
 import { DpsGraphData, DpsGraphDpsData, DpsGrapherResults } from 'src/app/model/dps-grapher/dps-grapher-results.model';
 import { Mode } from 'src/app/model/mode.enum';
 import { DamageSimService } from 'src/app/services/damage-sim.service';
@@ -17,8 +16,6 @@ export class DpsGrapherComponent {
 
   dpsGrapherResults: DpsGrapherResults;
 
-  chart: Chart;
-
   private graphColors = ['blue', 'orange', 'green', 'red', 'purple'];
 
   constructor(private damageSimservice: DamageSimService, private inputSetupService: InputSetupService) {}
@@ -32,44 +29,10 @@ export class DpsGrapherComponent {
       next: (results: DpsGrapherResults) => {
         this.loading = false;
         this.dpsGrapherResults = results;
-        // this.createChart(results.graph_data); //TODO graph style and size
       },
       error: (error) => {
         this.loading = false;
         this.dpsGrapherResults = { ...this.dpsGrapherResults, error: error.statusText };
-      },
-    });
-  }
-
-  createChart(dpsGraphData: DpsGraphData) {
-    this.chart?.destroy();
-    this.chart = new Chart('DpsGraph', {
-      type: 'line',
-      data: {
-        labels: dpsGraphData.x_values,
-        datasets: dpsGraphData.dps_data.map((dpsGraphDpsData: DpsGraphDpsData, index: number) => {
-          return {
-            label: dpsGraphDpsData.label,
-            data: dpsGraphDpsData.dps,
-            backgroundColor: this.graphColors[index],
-          };
-        }),
-      },
-      options: {
-        scales: {
-          x: {
-            title: {
-              display: true,
-              text: dpsGraphData.x_label,
-            },
-          },
-          y: {
-            title: {
-              display: true,
-              text: 'Dps',
-            },
-          },
-        },
       },
     });
   }
