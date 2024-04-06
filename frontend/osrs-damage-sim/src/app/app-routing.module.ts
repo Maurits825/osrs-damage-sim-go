a5cf2cdb18effc5d17934eac92f34d50cb5cb8cb
Maurits <63048736+Maurits825@users.noreply.github.com>
2024-04-06 14:01:24 +0200

    update routes to dps-calc and update github link

--- frontend/osrs-damage-sim/src/app/app-routing.module.ts
+++ frontend/osrs-damage-sim/src/app/app-routing.module.ts
@@ -3,9 +3,9 @@ import { RouterModule, Routes } from '@angular/router';
 import { DpsCalcComponent } from './core/dps-calc/dps-calc.component';
 
 const routes: Routes = [
-  { path: 'dmg-sim', component: DpsCalcComponent },
-  { path: '', redirectTo: '/dmg-sim', pathMatch: 'full' },
-  { path: '**', redirectTo: '/dmg-sim', pathMatch: 'full' },
+  { path: 'dps-calc', component: DpsCalcComponent },
+  { path: '', redirectTo: '/dps-calc', pathMatch: 'full' },
+  { path: '**', redirectTo: '/dps-calc', pathMatch: 'full' },
 ];
 
 @NgModule({
