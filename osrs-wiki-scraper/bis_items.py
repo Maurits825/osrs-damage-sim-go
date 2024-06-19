087607d90760cf891a758e07f0c163e201560ff8
Maurits <63048736+Maurits825@users.noreply.github.com>
2024-06-19 16:46:58 +0200

    group same stat items

--- osrs-wiki-scraper/bis_items.py
+++ osrs-wiki-scraper/bis_items.py
@@ -11,9 +11,15 @@ from graphviz import Digraph
 
 from constants import CACHE_DATA_FOLDER
 from generate_web_app_data import GenerateWebAppData
+from model.weapon_category import WeaponCategory
 
 ITEMS_DMG_SIM_JSON = CACHE_DATA_FOLDER / "items-dmg-sim.json"
 GEAR_SLOT_ITEM_JSON = CACHE_DATA_FOLDER / "gear_slot_items.json"
+
+# twisted, blorva, other stuff
+FILTER_IDS = [
+    "24664", "24666", "24668", "28254", "13199", "13197", "28256", "28258",
+]
 
 
 class Style(Enum):
@@ -32,7 +38,7 @@ STYLE_STATS = {
 
 @dataclass()
 class BisItem:
-    item_id: str
+    item_ids: List[str]
     next: List['BisItem']
 
 
@@ -62,7 +68,7 @@ class GenerateBisItems:
             Style.MAGIC: {},
         })
         for item_id, item in self.items.items():
-            if item_id == "11826":  # ARMA HELM BUG????
+            if item_id == "13195":  # TODO remove
                 a = 1
             if GenerateWebAppData.is_filtered_item(item, item_id):
                 continue
@@ -82,16 +88,19 @@ class GenerateBisItems:
             slot = item["slot"]
             for style in styles:
                 if slot not in bis_item_graph.items[style]:
-                    bis_item_graph.items[style][slot] = [BisItem(item_id, [])]
+                    bis_item_graph.items[style][slot] = [BisItem([item_id], [])]
                     continue
 
-                current_bis_item = BisItem("", bis_item_graph.items[style][slot])
-                self.insert_item_in_bis_list(style, current_bis_item, item_id)
+                current_bis_item = BisItem([], bis_item_graph.items[style][slot])
+                new_bis_item = BisItem([item_id], [])
+                self.insert_item_in_bis_list(style, current_bis_item, new_bis_item)
 
         img = self.create_graph_image(bis_item_graph)
         # img.show()
 
     def is_filtered_item(self, item, item_id):
+        if any(i == item_id for i in FILTER_IDS):
+            return True
         if "(perfected)" in item["name"]:
             return True
         if "(attuned)" in item["name"]:
@@ -116,13 +125,30 @@ class GenerateBisItems:
             return True
         if "(wrapped)" in item["name"]:
             return True
+        if "Wristbands of the arena" in item["name"]:
+            return True
 
         if "Ghommal's" in item["name"]:
+            return True
+        if "max cape" in item["name"]:
+            return True
+        if "slayer helmet (i)" in item["name"]:
             return True
         return False
 
     def get_item_style(self, item) -> list[Style]:
         styles = []
+        attack_styles, attack_type = GenerateWebAppData.get_attack_style_and_type(item)
+        if attack_type:
+            if item.get("str", 0) < 0 or item.get("rstr", 0) < 0 or item.get("mdmg", 0) < 0:
+                return []
+        if attack_type == "melee":
+            styles.append(Style.MELEE)
+        if attack_type == "ranged":
+            styles.append(Style.RANGED)
+        if attack_type == "magic":
+            styles.append(Style.MAGIC)
+
         for style in STYLE_STATS:
             zero_or_less = 0
             for stat in STYLE_STATS[style]:
@@ -131,49 +157,54 @@ class GenerateBisItems:
                     zero_or_less += 1
             if zero_or_less != len(STYLE_STATS[style]):
                 styles.append(style)
+            elif style in styles:
+                styles.remove(style)
 
         return styles
 
-    def insert_item_in_bis_list(self, style, current_bis_item: BisItem, new_item_id):
+    def insert_item_in_bis_list(self, style, current_bis_item: BisItem, new_bis_item: BisItem):
+        new_item_id = new_bis_item.item_ids[0]
         new_item = self.items[new_item_id]
         while len(current_bis_item.next) > 0:
             # first lets check how many up/down grades there are
             upgrades = []
             downgrades = []
             for next_bis_item in current_bis_item.next:
-                if self.is_item_upgrade(style, self.items[next_bis_item.item_id], new_item):
+                next_item = self.items[next_bis_item.item_ids[0]]
+                if self.compare_item(style, next_item, new_item, lambda i1, i2: i1 == i2):
+                    if new_item_id not in next_bis_item.item_ids:
+                        next_bis_item.item_ids.append(new_item_id)
+                    return
+                if self.compare_item(style, next_item, new_item, lambda i1, i2: i1 >= i2):
                     upgrades.append(next_bis_item)
-                if self.is_item_downgrade(style, self.items[next_bis_item.item_id], new_item):
+                if self.compare_item(style, next_item, new_item, lambda i1, i2: i1 <= i2):
                     downgrades.append(next_bis_item)
 
             up_count = len(upgrades)
             down_count = len(downgrades)
 
             if down_count == up_count == 0:
-                current_bis_item.next.append(BisItem(new_item_id, []))
+                current_bis_item.next.append(new_bis_item)
                 return
-            if down_count == up_count == 1 and upgrades[0] == downgrades[0]:
-                current_bis_item.next.remove(upgrades[0])
-                current_bis_item.next.append(BisItem(new_item_id, upgrades))
+            if down_count == up_count:
+                print("can this happen??")
                 return
 
             # only upgrades
             if down_count == 0:
-                if up_count == 0:
-                    current_bis_item.next.append(BisItem(new_item_id, []))
-                    return
                 for upgrade in upgrades:
                     current_bis_item.next.remove(upgrade)
-                current_bis_item.next.append(BisItem(new_item_id, upgrades))
+                new_bis_item.next = upgrades
+                current_bis_item.next.append(new_bis_item)
                 return
 
             # only downgrades
             if up_count == 0:
                 if down_count > 1:
-                    print("recursively go through downgrades......")
-                    # here we go
+                    # TODO???
+                    print("pls dont go here")
                     for downgrade in downgrades:
-                        self.insert_item_in_bis_list(style, downgrade, new_item_id)
+                        self.insert_item_in_bis_list(style, downgrade, new_bis_item)
                     return
                 else:
                     # only one downgrade so we travel down
@@ -182,25 +213,18 @@ class GenerateBisItems:
 
             # mix of up and downgrades, idk
             print("TODO ....")
-        current_bis_item.next.append(BisItem(new_item_id, []))
+        current_bis_item.next.append(new_bis_item)
 
-    def is_item_upgrade(self, style, item, new_item) -> bool:
+    def compare_item(self, style, item, new_item, compare) -> bool:
         true_count = 0
         for stat in STYLE_STATS[style]:
-            v = item.get(stat, 0)
             v_new = new_item.get(stat, 0)
-            if v_new >= v:
+            v = item.get(stat, 0)
+            if compare(v_new, v):
                 true_count += 1
-        return true_count == len(STYLE_STATS[style])
 
-    def is_item_downgrade(self, style, item, new_item) -> bool:
-        true_count = 0
-        for stat in STYLE_STATS[style]:
-            v = item.get(stat, 0)
-            v_new = new_item.get(stat, 0)
-            if v_new <= v:  # TODO same stats items will be randomly down/up grade of each other
-                true_count += 1
-        return true_count == len(STYLE_STATS[style])
+        attack_speed = item.get('aspeed', 0) == new_item.get('aspeed', 0)
+        return true_count == len(STYLE_STATS[style]) and attack_speed
 
     def create_graph_image(self, bis_graph: BisItemsGraph):
         width = 5000
@@ -214,8 +238,6 @@ class GenerateBisItems:
         for style in [Style.MELEE, Style.RANGED, Style.MAGIC]:  # TODO other styles
             for slot in bis_graph.items[style]:
                 print("slot: " + str(slot))
-                if slot == 4:
-                    a = 2
                 items = self.gear_slot_items[str(slot)]
                 # bis_items = bis_graph.items[style][slot]
                 self.create_spring_graph_image(bis_graph.items[style][slot],
@@ -229,47 +251,48 @@ class GenerateBisItems:
         img_dir = 'img'
 
         # Dictionary to hold item_id to BisItem mappings
-        item_dict = {item.item_id: item for item in items}
+        # item_dict = {item.item_id: item for item in items} # TODO remove?
 
         # Set to keep track of visited nodes
         visited = set()
 
         # Helper function to add nodes and edges
         def add_nodes_and_edges(item: BisItem):
-            if item.item_id == "11832":
-                a = 2
-            if item.item_id in visited:
+            if item.item_ids[0] in visited:
                 return
-            visited.add(item.item_id)
+            visited.add(item.item_ids[0])
 
-            # Add the node with an image placeholder
-            icon_base64 = self.get_icon(item.item_id)
-            if icon_base64 is None:
-                print("skip: " + str(item.item_id))
-                return
+            label_html = ""
+            icon_paths = []
+            icons_html = ""
+            for item_id in item.item_ids:
+                icon_base64 = self.get_icon(item_id)
+                if icon_base64 is None:
+                    print("skip: " + str(item_id))
+                    return
 
-            try:
-                icon_image = Image.open(io.BytesIO(base64.b64decode(icon_base64)))
-            except Exception:
-                print("skip: " + str(item.item_id))
-                return
+                try:
+                    icon_image = Image.open(io.BytesIO(base64.b64decode(icon_base64)))
+                except Exception:
+                    print("skip: " + str(item_id))
+                    return
 
-            icon_path = os.path.join(img_dir, f'{item.item_id}.png')
-            icon_image.save(os.path.join("graphs", icon_path))
+                icon_path = os.path.join(img_dir, f'{item_id}.png')
+                icon_paths.append(icon_path)
+                icon_image.save(os.path.join("graphs", icon_path))
 
-            label_text = self.items[item.item_id]["name"] + "(" + str(item.item_id) + ")"
-            dot.node(item.item_id, label=(
-                    '<'
-                    '<table border="0" cellborder="0">'
-                    '  <tr><td><img src="' + icon_path + '"/></td></tr>'
-                                                         '  <tr><td>' + label_text + '</td></tr>'
-                                                                                     '</table>'
-                                                                                     '>'
+                label_html += '<td>' + self.items[item_id]["name"] + "(" + str(item_id) + ")" + '</td>'
+                icons_html += '<td><img src="' + icon_path + '"/></td>'
+
+            dot.node(item.item_ids[0], label=(
+                    '<<table border="1" cellborder="0" cellspacing="0">'
+                    '<tr>' + icons_html + '</tr>' +
+                    '<tr>' + label_html + '</tr></table>>'
             ), shape='plain')
 
             if item.next:
                 for next_item in item.next:
-                    dot.edge(item.item_id, next_item.item_id)
+                    dot.edge(item.item_ids[0], next_item.item_ids[0])
                     add_nodes_and_edges(next_item)
 
         # Add nodes and edges for all items
