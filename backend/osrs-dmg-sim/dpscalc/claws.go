640af1f7379d2f8b064eebf7ed371ec727a32a83
Maurits <63048736+Maurits825@users.noreply.github.com>
2024-07-17 18:54:13 +0200

    refactor some claws fns

--- backend/osrs-dmg-sim/dpscalc/claws.go
+++ backend/osrs-dmg-sim/dpscalc/claws.go
@@ -7,7 +7,7 @@ import (
 )
 
 func getDragonClawsSpecDist(accuracy float64, maxHit int) *attackdist.AttackDistribution {
-	allHits := getDragonClawsSpecHits(accuracy, maxHit)
+	allHits := getClawsSpecHits(accuracy, maxHit, 4, -1, dragonClawsHits)
 	missAccuracy := math.Pow(1-accuracy, 4)
 	allHits = append(allHits, attackdist.WeightedHit{Probability: missAccuracy * 1 / 3, Hitsplats: []int{0, 0, 0, 0}})
 	allHits = append(allHits, attackdist.WeightedHit{Probability: missAccuracy * 2 / 3, Hitsplats: []int{0, 0, 1, 1}})
@@ -15,7 +15,7 @@ func getDragonClawsSpecDist(accuracy float64, maxHit int) *attackdist.AttackDist
 }
 
 func getBurningClawsSpecDist(accuracy float64, maxHit int) *attackdist.AttackDistribution {
-	allHits := getBurningClawsSpecHits(accuracy, maxHit)
+	allHits := getClawsSpecHits(accuracy, maxHit, 3, 0, burningClawsHits)
 	missAccuracy := math.Pow(1-accuracy, 3)
 	allHits = append(allHits, attackdist.WeightedHit{Probability: missAccuracy / 5, Hitsplats: []int{0, 0, 0}})
 	allHits = append(allHits, attackdist.WeightedHit{Probability: 2 * missAccuracy / 5, Hitsplats: []int{1, 0, 0}})
@@ -30,83 +30,62 @@ func getClawsMinMaxHits(roll int, totalRolls int, accuracy float64, baseMaxhit f
 	return
 }
 
-var dragonClawsHits = []func(baseHit int) (h1, h2, h3, h4 int){
-	func(baseHit int) (h1, h2, h3, h4 int) {
-		h1 = int(baseHit / 2)
-		h2 = int(baseHit / 4)
-		h3 = int(baseHit / 8)
-		h4 = int(baseHit/8) + 1
-		return
-	},
-	func(baseHit int) (h1, h2, h3, h4 int) {
-		h1 = 0
-		h2 = int(baseHit / 2)
-		h3 = int(baseHit / 4)
-		h4 = int(baseHit/4) + 1
-		return
-	},
-	func(baseHit int) (h1, h2, h3, h4 int) {
-		h1 = 0
-		h2 = 0
-		h3 = int(baseHit / 2)
-		h4 = int(baseHit/2) + 1
-		return
-	},
-	func(baseHit int) (h1, h2, h3, h4 int) {
-		h1 = 0
-		h2 = 0
-		h3 = 0
-		h4 = baseHit + 1
-		return
-	},
-}
-
-var burningClawsHits = []func(baseHit int) (h1, h2, h3 int){
-	func(baseHit int) (h1, h2, h3 int) {
-		h1 = int(baseHit / 2)
-		h2 = int(baseHit / 4)
-		h3 = int(baseHit / 4)
-		return
-	},
-	func(baseHit int) (h1, h2, h3 int) {
-		h1 = 2
-		h2 = int(baseHit/2) - 1
-		h3 = int(baseHit/2) - 1
-		return
-	},
-	func(baseHit int) (h1, h2, h3 int) {
-		h1 = 1
-		h2 = 1
-		h3 = int(baseHit - 2)
-		return
-	},
-}
-
-func getDragonClawsSpecHits(baseAccuracy float64, baseMaxHit int) []attackdist.WeightedHit {
-	allHits := make([]attackdist.WeightedHit, 0)
-	totalRolls := 4
-	for rollHit := 0; rollHit < totalRolls; rollHit++ {
-		prob, minHit, maxHit := getClawsMinMaxHits(rollHit, totalRolls, baseAccuracy, float32(baseMaxHit), -1)
-		hitsplats := make([]attackdist.WeightedHit, maxHit-minHit+1)
-		for hit := minHit; hit <= maxHit; hit++ {
-			hit1, hit2, hit3, hit4 := dragonClawsHits[rollHit](hit)
-			hitsplats[hit-minHit] = attackdist.WeightedHit{Probability: float64(prob), Hitsplats: []int{hit1, hit2, hit3, hit4}}
+var dragonClawsHits = []func(baseHit int) []int{
+	func(baseHit int) []int {
+		return []int{
+			int(baseHit / 2),
+			int(baseHit / 4),
+			int(baseHit / 8),
+			int(baseHit/8) + 1,
 		}
-		allHits = append(allHits, hitsplats...)
-	}
-
-	return allHits
+	},
+	func(baseHit int) []int {
+		return []int{0,
+			int(baseHit / 2),
+			int(baseHit / 4),
+			int(baseHit/4) + 1,
+		}
+	},
+	func(baseHit int) []int {
+		return []int{
+			0,
+			0,
+			int(baseHit / 2),
+			int(baseHit/2) + 1,
+		}
+	},
+	func(baseHit int) []int {
+		return []int{0, 0, 0, baseHit + 1}
+	},
 }
 
-func getBurningClawsSpecHits(baseAccuracy float64, baseMaxHit int) []attackdist.WeightedHit {
+var burningClawsHits = []func(baseHit int) []int{
+	func(baseHit int) []int {
+		return []int{
+			int(baseHit / 2),
+			int(baseHit / 4),
+			int(baseHit / 4),
+		}
+	},
+	func(baseHit int) []int {
+		return []int{2,
+			int(baseHit/2) - 1,
+			int(baseHit/2) - 1,
+		}
+	},
+	func(baseHit int) []int {
+		return []int{1, 1, int(baseHit - 2)}
+	},
+}
+
+func getClawsSpecHits(baseAccuracy float64, baseMaxHit int, totalRolls int, highOffset int, minMaxHits []func(int) []int) []attackdist.WeightedHit {
 	allHits := make([]attackdist.WeightedHit, 0)
-	totalRolls := 3
 	for rollHit := 0; rollHit < totalRolls; rollHit++ {
-		prob, minHit, maxHit := getClawsMinMaxHits(rollHit, totalRolls, baseAccuracy, float32(baseMaxHit), 0)
+		prob, minHit, maxHit := getClawsMinMaxHits(rollHit, totalRolls, baseAccuracy, float32(baseMaxHit), highOffset)
 		hitsplats := make([]attackdist.WeightedHit, maxHit-minHit+1)
 		for hit := minHit; hit <= maxHit; hit++ {
-			hit1, hit2, hit3 := burningClawsHits[rollHit](hit)
-			hitsplats[hit-minHit] = attackdist.WeightedHit{Probability: float64(prob), Hitsplats: []int{hit1, hit2, hit3}}
+			hits := minMaxHits[rollHit](hit)
+			hitsplats[hit-minHit] = attackdist.WeightedHit{Probability: float64(prob), Hitsplats: hits}
 		}
 		allHits = append(allHits, hitsplats...)
 	}
