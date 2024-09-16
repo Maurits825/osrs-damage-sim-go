19b757d881f8603e563fa4b2278ad0c4536efc3a
Maurits <63048736+Maurits825@users.noreply.github.com>
2024-09-16 13:20:17 +0200

    refactor changelog to list

---
+++ frontend/osrs-damage-sim/src/app/shared/modals/changelog-modal/changelog.const.ts
@@ -0,0 +1,13 @@
+interface logData {
+  title: string;
+  body: string[];
+}
+
+export const changeLogData: logData[] = [
+  { title: '16 September 2024', body: ['Added TTK and expected hit columns'] },
+  { title: '29 August 2024', body: ['Araxxor update'] },
+  { title: '18 July 2024', body: ['Added scorching bow, emberlight & burning claws', 'Updated dragon claw spec calc'] },
+  { title: '26 June 2024', body: ['Moved result graphs in a tab view'] },
+  { title: '12 June 2024', body: ['Added changelog', 'Added wiki dps setup button'] },
+  { title: '10 June 2024', body: ['Added most combat rebalance changes'] },
+];
