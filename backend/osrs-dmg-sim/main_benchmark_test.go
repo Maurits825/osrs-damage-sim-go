93f54472a8ac1af3624ee4e2ee10b4791e5a1332
Maurits <63048736+Maurits825@users.noreply.github.com>
2024-09-19 11:05:26 +0200

    fix main benchmark test

--- backend/osrs-dmg-sim/main_benchmark_test.go
+++ backend/osrs-dmg-sim/main_benchmark_test.go
@@ -1,47 +1,21 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"io"
-	"os"
-	"path/filepath"
 	"testing"
 
 	"github.com/Maurits825/osrs-damage-sim-go/backend/osrs-damage-sim/dpscalc"
 	"github.com/Maurits825/osrs-damage-sim-go/backend/osrs-damage-sim/dpsgrapher"
+	"github.com/Maurits825/osrs-damage-sim-go/backend/osrs-damage-sim/testutil"
 )
 
 type testInputSetups map[string]testInputSetup
 
 type testInputSetup struct {
-	ExpectedDps float32            `json:"expectedDps"`
-	InputSetup  dpscalc.InputSetup `json:"inputSetup"`
-}
-
-func loadTestInputSetups(file string) testInputSetups {
-	filePath := filepath.Join("testdata", file)
-	inputSetupFile, err := os.Open(filePath)
-
-	if err != nil {
-		fmt.Println(err)
-		return testInputSetups{}
-	}
-
-	defer inputSetupFile.Close()
-
-	byteValue, _ := io.ReadAll(inputSetupFile)
-	var testInputSetups testInputSetups
-	if err := json.Unmarshal(byteValue, &testInputSetups); err != nil {
-		fmt.Println("Error decoding inputsetup JSON:", err)
-		return nil
-	}
-
-	return testInputSetups
+	InputSetup dpscalc.InputSetup `json:"inputSetup"`
 }
 
 func BenchmarkMainDpsCalc(b *testing.B) {
-	inputSetups := loadTestInputSetups("benchmark_setups.json")
+	inputSetups := *testutil.LoadTestFile[testInputSetups]("benchmark_setups.json")
 	for setupName, testSetup := range inputSetups {
 		b.Run(setupName, func(b *testing.B) {
 			for i := 0; i < b.N; i++ {
