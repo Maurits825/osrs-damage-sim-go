140d7fe6851a7fd5cc50eaa25caf433a1cb6e1c5
Maurits <63048736+Maurits825@users.noreply.github.com>
2025-03-13 17:29:07 +0100

    more multi npc presets

--- frontend/osrs-damage-sim/src/app/shared/components/multi-npc-input/multi-npc.model.ts
+++ frontend/osrs-damage-sim/src/app/shared/components/multi-npc-input/multi-npc.model.ts
@@ -23,9 +23,33 @@ export const COX_MAGE_RANGE_NPC_IDS: MultiNpcPreset = {
   ids: ['7584', '7573', '7529', '7527', '7528', '7530', '7560', '7559', '7566', '7604', '7562', '7561'],
 };
 
+export const TOA_NPC_IDS: MultiNpcPreset = {
+  name: 'Tombs of Amascut',
+  ids: ['11778', '11719', '11730', '11789', '11797', '11753', '11755', '11761'],
+};
+
+export const INFERNO_NPC_IDS: MultiNpcPreset = {
+  name: 'Inferno',
+  ids: ['7692', '7693', '7697', '7698', '7699', '7700', '7706'],
+};
+
+export const COLO_NPC_IDS: MultiNpcPreset = {
+  name: 'Colosseum',
+  ids: ['12811', '12810', '12817', '12818', '12819', '12812', '12821'],
+};
+
+export const NEX_NPC_IDS: MultiNpcPreset = {
+  name: 'Nex',
+  ids: ['11278', '11283', '11284', '11285', '11286'],
+};
+
 export const ALL_MULTI_NPC_PRESETS: MultiNpcPreset[] = [
   TOB_NORMAL_NPC_IDS,
   TOB_HARD_NPC_IDS,
   COX_MELEE_NPC_IDS,
   COX_MAGE_RANGE_NPC_IDS,
+  TOA_NPC_IDS,
+  INFERNO_NPC_IDS,
+  COLO_NPC_IDS,
+  NEX_NPC_IDS,
 ];
