fabd9cdc9d290d2038584132133c56d697690220
Maurits <63048736+Maurits825@users.noreply.github.com>
2025-03-14 19:21:09 +0100

    added soul armour, treads and confliction in quick gear and set select

--- frontend/osrs-damage-sim/src/app/shared/components/quick-gear-select/quick-gear-select.component.ts
+++ frontend/osrs-damage-sim/src/app/shared/components/quick-gear-select/quick-gear-select.component.ts
@@ -1,15 +1,17 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { GearSlot } from 'src/app/model/osrs/gear-slot.enum';
-import { AttackType, Item } from 'src/app/model/osrs/item.model';
-import { QuickGearSlots } from 'src/app/model/shared/quick-gear.model';
+import { allAttackTypes, AttackType, Item } from 'src/app/model/osrs/item.model';
+import { QuickGear, QuickGearSlots } from 'src/app/model/shared/quick-gear.model';
 import { StaticDataService } from 'src/app/services/static-data.service';
+import { quickGearSetups } from './quick-gear.const';
+import { ItemService } from 'src/app/services/item.service';
 
 @Component({
   selector: 'app-quick-gear-select',
   templateUrl: './quick-gear-select.component.html',
   styleUrls: ['./quick-gear-select.component.css'],
 })
-export class QuickGearSelectComponent {
+export class QuickGearSelectComponent implements OnInit {
   @Input()
   gearSlot: GearSlot;
 
@@ -21,12 +23,24 @@ export class QuickGearSelectComponent {
 
   GearSlot = GearSlot;
 
-  quickGearSlots: QuickGearSlots;
+  quickGearSlots: QuickGearSlots = {} as QuickGearSlots;
 
-  constructor(private staticDataService: StaticDataService) {
-    this.staticDataService.quickGearSlots$.subscribe((quickGearSlots: QuickGearSlots) => {
-      this.quickGearSlots = quickGearSlots;
-    });
+  constructor(private itemService: ItemService) {}
+
+  ngOnInit(): void {
+    for (const gearSlot in GearSlot) {
+      const slot = GearSlot[gearSlot as keyof typeof GearSlot] as keyof QuickGearSlots;
+      this.quickGearSlots[slot] = {} as QuickGear;
+      for (const attackIdx in allAttackTypes) {
+        const items: Item[] = [];
+        const attackType = allAttackTypes[attackIdx];
+        for (const itemId of (quickGearSetups as any)[slot][attackType]) {
+          const item = this.itemService.getItem(slot, itemId);
+          items.push(item);
+        }
+        (this.quickGearSlots as any)[slot][attackType] = items;
+      }
+    }
   }
 
   onGearSelect(item: Item): void {
