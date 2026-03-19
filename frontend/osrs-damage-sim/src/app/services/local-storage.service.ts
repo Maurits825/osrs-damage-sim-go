f59df3816402b57afbce1a05ed22b88680a3eb9d
Maurits <63048736+Maurits825@users.noreply.github.com>
2026-03-19 13:31:51 +0100

    add input setups presets

--- frontend/osrs-damage-sim/src/app/services/local-storage.service.ts
+++ frontend/osrs-damage-sim/src/app/services/local-storage.service.ts
@@ -5,22 +5,31 @@ import { GearSetupPreset } from '../model/shared/gear-preset.model';
 import { GearSlot } from '../model/osrs/gear-slot.enum';
 import { DEFAULT_USER_SETTINGS, UserSettings } from '../model/shared/user-settings.model';
 import { GearSetup } from '../model/shared/gear-setup.model';
+import { InputGearSetup, InputGearSetupPreset } from '../model/dps-calc/input-setup.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class LocalStorageService {
   public gearSetupWatch$: Observable<GearSetupPreset[]>;
+  public inputPresetWatch$: Observable<InputGearSetupPreset[]>;
   public userSettingsWatch$: Observable<UserSettings>;
   public userSettings$: BehaviorSubject<UserSettings> = new BehaviorSubject<UserSettings>(DEFAULT_USER_SETTINGS);
 
   private gearSetupKey = 'gearSetup';
+  private inputPresetKey = 'inputPreset';
   private userSettingsKey = 'userSettings';
 
   constructor(private storage: StorageMap) {
     this.gearSetupWatch$ = this.storage.watch(this.gearSetupKey).pipe(
       map((gearSetups) => gearSetups ?? []),
       map((gearSetups) => gearSetups as GearSetupPreset[]),
+      shareReplay(1),
+    );
+
+    this.inputPresetWatch$ = this.storage.watch(this.inputPresetKey).pipe(
+      map((presets) => presets ?? []),
+      map((presets) => presets as InputGearSetupPreset[]),
       shareReplay(1),
     );
 
@@ -37,23 +46,38 @@ export class LocalStorageService {
 
     const gearSetupPreset = this.createGearSetupPreset(gearSetup);
 
-    return this.getSavedGearSetups().pipe(
-      map((gearSetups: GearSetupPreset[]) =>
-        gearSetups.filter((gearSetup: GearSetupPreset) => gearSetup.name !== gearSetupPreset.name),
-      ),
+    return this.getKeyList<GearSetupPreset>(this.gearSetupKey).pipe(
+      map(this.nameFilter<GearSetupPreset>(gearSetupPreset.name)),
       switchMap((gearSetups: GearSetupPreset[]) =>
         this.storage.set(this.gearSetupKey, [...gearSetups, gearSetupPreset]),
       ),
     );
   }
 
+  public saveInputPreset(preset: InputGearSetupPreset): Observable<string> {
+    const error = this.validatedInputPreset(preset);
+    if (error) return of(error);
+
+    return this.getKeyList<InputGearSetupPreset>(this.inputPresetKey).pipe(
+      map(this.nameFilter<InputGearSetupPreset>(preset.name)),
+      switchMap((presets: InputGearSetupPreset[]) => this.storage.set(this.inputPresetKey, [...presets, preset])),
+    );
+  }
+
   public deleteGearSetup(setupName: string): void {
-    this.getSavedGearSetups()
+    this.getKeyList<GearSetupPreset>(this.gearSetupKey)
       .pipe(
-        map((gearSetups: GearSetupPreset[]) =>
-          gearSetups.filter((gearSetup: GearSetupPreset) => gearSetup.name !== setupName),
-        ),
+        map(this.nameFilter<GearSetupPreset>(setupName)),
         switchMap((gearSetups: GearSetupPreset[]) => this.storage.set(this.gearSetupKey, gearSetups)),
+      )
+      .subscribe();
+  }
+
+  public deleteInputPreset(setupName: string): void {
+    this.getKeyList<InputGearSetupPreset>(this.inputPresetKey)
+      .pipe(
+        map(this.nameFilter<InputGearSetupPreset>(setupName)),
+        switchMap((presets: InputGearSetupPreset[]) => this.storage.set(this.inputPresetKey, presets)),
       )
       .subscribe();
   }
@@ -62,20 +86,40 @@ export class LocalStorageService {
     this.storage.set(this.gearSetupKey, []).subscribe();
   }
 
+  public deleteAllInputPresets(): void {
+    this.storage.set(this.inputPresetKey, []).subscribe();
+  }
+
   public saveUserSettings(userSettings: UserSettings): void {
     this.storage.set(this.userSettingsKey, userSettings).subscribe();
   }
 
-  private getSavedGearSetups(): Observable<GearSetupPreset[]> {
-    return this.storage.get(this.gearSetupKey).pipe(
-      map((gearSetups) => gearSetups ?? []),
-      map((gearSetups) => gearSetups as GearSetupPreset[]),
+  private nameFilter<T extends { name: string }>(filterName: string) {
+    return (items: T[]): T[] => items.filter((item) => item.name !== filterName);
+  }
+
+  private getKeyList<T>(key: string): Observable<T[]> {
+    return this.storage.get(key).pipe(
+      map((arr) => arr ?? []),
+      map((arr) => arr as T[]),
     );
   }
 
   private validatedGearSetup(gearSetup: GearSetup): string | null {
     if (!gearSetup.setupName) return 'Empty setup name';
     if (!Object.values(gearSetup.gear).some((item) => !!item)) return 'No gear selected';
+
+    return null;
+  }
+
+  private validatedInputPreset(preset: InputGearSetupPreset): string | null {
+    if (!preset.name) return 'Empty preset name';
+    if (preset.inputGearSetups.length == 0) return 'No setups';
+
+    for (let i = 0; i < preset.inputGearSetups.length; i++) {
+      const error = this.validatedGearSetup(preset.inputGearSetups[i].gearSetup);
+      if (error) return 'Setup ' + +(i + 1) + ': ' + error;
+    }
 
     return null;
   }
